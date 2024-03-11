4b05bfb7e6bd1dde354b77acabaff1ae804a33c0
Błażej Bartoszewski <blazejbart@gmail.com>
2024-03-11 14:43:15 +0100

    add API herokuapp to contactsSlice.js

--- src/redux/contactsSlice.js
+++ src/redux/contactsSlice.js
@@ -1,9 +1,71 @@
+// import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+// import axios from 'axios';
+
+// const apiUrl = 'https://65d5f0ccf6967ba8e3bd06c0.mockapi.io/contacts/contacts';
+
+// export const fetchContacts = createAsyncThunk(
+//   'contacts/fetchContacts',
+//   async (_, { getState }) => {
+//     const { auth } = getState();
+//     const response = await axios.get(apiUrl, {
+//       headers: {
+//         Authorization: `Bearer ${auth.token}`,
+//       },
+//     });
+//     return response.data;
+//   }
+// );
+
+// export const addNewContact = createAsyncThunk(
+//   'contacts/addNewContact',
+//   async (contact, { getState }) => {
+//     const { auth } = getState();
+//     const response = await axios.post(apiUrl, contact, {
+//       headers: {
+//         Authorization: `Bearer ${auth.token}`,
+//       },
+//     });
+//     return response.data;
+//   }
+// );
+
+// export const deleteContact = createAsyncThunk(
+//   'contacts/deleteContact',
+//   async (id, { getState }) => {
+//     const { auth } = getState();
+//     await axios.delete(`${apiUrl}/${id}`, {
+//       headers: {
+//         Authorization: `Bearer ${auth.token}`,
+//       },
+//     });
+//     return id;
+//   }
+// );
+
+// const contactsSlice = createSlice({
+//   name: 'contacts',
+//   initialState: [],
+//   reducers: {},
+//   extraReducers: builder => {
+//     builder.addCase(fetchContacts.fulfilled, (state, action) => {
+//       return action.payload;
+//     });
+//     builder.addCase(addNewContact.fulfilled, (state, action) => {
+//       state.push(action.payload);
+//     });
+//     builder.addCase(deleteContact.fulfilled, (state, action) => {
+//       return state.filter(contact => contact.id !== action.payload);
+//     });
+//   },
+// });
+
+// export default contactsSlice.reducer;
+
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 import axios from 'axios';
 
-const apiUrl = 'https://65d5f0ccf6967ba8e3bd06c0.mockapi.io/contacts/contacts';
+const apiUrl = 'https://connections-api.herokuapp.com/contacts';
 
-// Tworzenie createAsyncThunk dla fetchContacts
 export const fetchContacts = createAsyncThunk(
   'contacts/fetchContacts',
   async (_, { getState }) => {
@@ -17,7 +79,6 @@ export const fetchContacts = createAsyncThunk(
   }
 );
 
-// Tworzenie createAsyncThunk dla addNewContact
 export const addNewContact = createAsyncThunk(
   'contacts/addNewContact',
   async (contact, { getState }) => {
@@ -31,17 +92,16 @@ export const addNewContact = createAsyncThunk(
   }
 );
 
-// Tworzenie createAsyncThunk dla deleteContact
 export const deleteContact = createAsyncThunk(
   'contacts/deleteContact',
-  async (id, { getState }) => {
+  async (contactId, { getState }) => {
     const { auth } = getState();
-    await axios.delete(`${apiUrl}/${id}`, {
+    await axios.delete(`${apiUrl}/${contactId}`, {
       headers: {
         Authorization: `Bearer ${auth.token}`,
       },
     });
-    return id;
+    return contactId;
   }
 );
 
