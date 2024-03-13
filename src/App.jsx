37113b57936556eb826ed7e26dbb5ada925a8697
Błażej Bartoszewski <blazejbart@gmail.com>
2024-03-13 20:56:40 +0100

    fixed authSlice.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,49 @@
+// import React, { useEffect } from 'react';
+// import { useDispatch } from 'react-redux';
+// import { Routes, Route } from 'react-router-dom';
+// import Layout from './components/LayoutPage/Layout';
+// import PrivateRoute from './components/PrivateRoute';
+// import RestrictedRoute from './components/RestrictedRoute';
+// import { refreshToken } from './redux/auth/authSlice';
+// import { useAuth } from './hooks';
+// import Home from './pages/HomePage/Home';
+// import Register from './pages/Register';
+// import Login from './pages/Login';
+// import Phonebook from './pages/Phonebook';
+
+// const App = () => {
+//   const { isLoggedIn } = useAuth();
+//   const dispatch = useDispatch();
+
+//   useEffect(() => {
+//     dispatch(refreshToken());
+//   }, [dispatch]);
+
+//   return (
+//     <Routes>
+//       <Route path="/" element={<Layout />}>
+//         <Route index element={<Home />} />
+//         <Route
+//           path="/register"
+//           element={<RestrictedRoute component={<Register />} redirectTo="/" />}
+//         />
+//         <Route
+//           path="/login"
+//           element={<RestrictedRoute component={<Login />} redirectTo="/" />}
+//         />
+//         <Route
+//           path="/phonebook"
+//           element={
+//             <PrivateRoute redirectTo="/login" component={<Phonebook />} />
+//           }
+//         />
+//       </Route>
+//     </Routes>
+//   );
+// };
+
+// export default App;
+
 import React, { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import { Routes, Route } from 'react-router-dom';
@@ -6,7 +52,6 @@ import PrivateRoute from './components/PrivateRoute';
 import RestrictedRoute from './components/RestrictedRoute';
 import { refreshToken } from './redux/auth/authSlice';
 import { useAuth } from './hooks';
-import { fetchContacts } from './redux/contactsSlice';
 import Home from './pages/HomePage/Home';
 import Register from './pages/Register';
 import Login from './pages/Login';
@@ -18,8 +63,7 @@ const App = () => {
 
   useEffect(() => {
     dispatch(refreshToken());
-    if (isLoggedIn) dispatch(fetchContacts());
-  }, [dispatch, isLoggedIn]);
+  }, [dispatch]);
 
   return (
     <Routes>
@@ -33,12 +77,15 @@ const App = () => {
           path="/login"
           element={<RestrictedRoute component={<Login />} redirectTo="/" />}
         />
-        <Route
-          path="/phonebook"
-          element={
-            <PrivateRoute redirectTo="/login" component={<Phonebook />} />
-          }
-        />
+        {/* Warunkowe wyrenderowanie komponentu Phonebook na podstawie stanu isLoggedIn */}
+        {isLoggedIn && (
+          <Route
+            path="/phonebook"
+            element={
+              <PrivateRoute redirectTo="/login" component={<Phonebook />} />
+            }
+          />
+        )}
       </Route>
     </Routes>
   );
--- src/redux/auth/authSlice.js
+++ src/redux/auth/authSlice.js
@@ -1,216 +1,8 @@
-// import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
-// import axios from 'axios';
-
-// const setAuthToken = token => {
-//   if (token) {
-//     axios.defaults.headers.common.Authorization = `Bearer ${token}`;
-//   } else {
-//     delete axios.defaults.headers.common.Authorization;
-//   }
-// };
-
-// export const loginUser = createAsyncThunk(
-//   'auth/loginUser',
-//   async (userData, { dispatch, rejectWithValue }) => {
-//     try {
-//       const response = await axios.post(
-//         'https://connections-api.herokuapp.com/users/login',
-//         userData
-//       );
-//       const token = response.data.token;
-//       localStorage.setItem('token', token);
-//       setAuthToken(token);
-//       if (response.status === 200) {
-//         const user = response.data.user;
-//         dispatch(loginUserSuccess(user));
-//       } else {
-//         dispatch(loginUserFailure('Login failed'));
-//       }
-//     } catch (error) {
-//       if (!error.response) {
-//         return rejectWithValue('No response from server');
-//       }
-//       const status = error.response.status;
-//       if (status === 401 || status === 403) {
-//         return rejectWithValue('Unauthorized access');
-//       }
-//       return rejectWithValue(error.message);
-//     }
-//   }
-// );
-
-// export const registerUser = createAsyncThunk(
-//   'auth/registerUser',
-//   async (userData, { dispatch, rejectWithValue }) => {
-//     try {
-//       const response = await axios.post(
-//         'https://connections-api.herokuapp.com/users/signup',
-//         userData
-//       );
-//       if (response.status === 201) {
-//         const user = response.data.user;
-//         const token = response.data.token;
-//         localStorage.setItem('token', token);
-//         setAuthToken(token);
-//         dispatch(loginUserSuccess(user));
-//       } else {
-//         dispatch(loginUserFailure('Registration failed'));
-//       }
-//     } catch (error) {
-//       if (!error.response) {
-//         return rejectWithValue('No response from server');
-//       }
-//       const status = error.response.status;
-//       if (status === 401 || status === 403) {
-//         return rejectWithValue('Unauthorized access');
-//       }
-//       return rejectWithValue(error.message);
-//     }
-//   }
-// );
-
-// export const logoutUser = createAsyncThunk(
-//   'auth/logoutUser',
-//   async (_, { dispatch, rejectWithValue }) => {
-//     try {
-//       const response = await axios.post(
-//         'https://connections-api.herokuapp.com/users/logout'
-//       );
-//       if (response.status === 200) {
-//         localStorage.removeItem('token');
-//         setAuthToken(null);
-//         dispatch(logoutUserSuccess());
-//       } else {
-//         dispatch(logoutUserFailure('Logout failed'));
-//       }
-//     } catch (error) {
-//       if (!error.response) {
-//         return rejectWithValue('No response from server');
-//       }
-//       const status = error.response.status;
-//       if (status === 401 || status === 403) {
-//         return rejectWithValue('Unauthorized access');
-//       }
-//       return rejectWithValue(error.message);
-//     }
-//   }
-// );
-
-// export const refreshToken = createAsyncThunk(
-//   'auth/refreshToken',
-//   async (_, { dispatch, getState, rejectWithValue }) => {
-//     const token = getState().auth.token;
-//     if (!token) {
-//       return rejectWithValue('No token available');
-//     }
-//     try {
-//       const response = await axios.get(
-//         'https://connections-api.herokuapp.com/users/current'
-//       );
-//       if (response.status === 200) {
-//         const user = response.data.user;
-//         dispatch(loginUserSuccess(user));
-//       } else {
-//         dispatch(logoutUser());
-//       }
-//     } catch (error) {
-//       dispatch(logoutUser());
-//     }
-//   }
-// );
-
-// const initialState = {
-//   user: null,
-//   isLoggedIn: false,
-//   loading: false,
-//   error: null,
-//   token: localStorage.getItem('token'),
-// };
-
-// const authSlice = createSlice({
-//   name: 'auth',
-//   initialState,
-//   reducers: {
-//     loginUserStart(state) {
-//       state.loading = true;
-//       state.error = null;
-//     },
-//     loginUserSuccess(state, action) {
-//       state.loading = false;
-//       state.isLoggedIn = true;
-//       state.user = action.payload;
-//       state.error = null;
-//     },
-//     loginUserFailure(state, action) {
-//       state.loading = false;
-//       state.error = action.payload;
-//     },
-//     logoutUserStart(state) {
-//       state.loading = true;
-//       state.error = null;
-//     },
-//     logoutUserSuccess(state) {
-//       state.loading = false;
-//       state.isLoggedIn = false;
-//       state.user = null;
-//       state.error = null;
-//     },
-//     logoutUserFailure(state, action) {
-//       state.loading = false;
-//       state.error = action.payload;
-//     },
-//   },
-//   extraReducers: builder => {
-//     builder.addCase(loginUser.pending, state => {
-//       state.loading = true;
-//     });
-//     builder.addCase(loginUser.fulfilled, state => {
-//       state.loading = false;
-//     });
-//     builder.addCase(loginUser.rejected, (state, action) => {
-//       state.loading = false;
-//       state.error = action.error.message;
-//     });
-//     builder.addCase(registerUser.pending, state => {
-//       state.loading = true;
-//     });
-//     builder.addCase(registerUser.fulfilled, state => {
-//       state.loading = false;
-//     });
-//     builder.addCase(registerUser.rejected, (state, action) => {
-//       state.loading = false;
-//       state.error = action.error.message;
-//     });
-//     builder.addCase(logoutUser.pending, state => {
-//       state.loading = true;
-//     });
-//     builder.addCase(logoutUser.fulfilled, state => {
-//       state.loading = false;
-//     });
-//     builder.addCase(logoutUser.rejected, (state, action) => {
-//       state.loading = false;
-//       state.error = action.error.message;
-//     });
-//   },
-// });
-
-// export const {
-//   loginUserStart,
-//   loginUserSuccess,
-//   loginUserFailure,
-//   logoutUserStart,
-//   logoutUserSuccess,
-//   logoutUserFailure,
-// } = authSlice.actions;
-
-// export default authSlice.reducer;
-
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 import axios from 'axios';
 
 const apiUrl = 'https://connections-api.herokuapp.com/users';
 
-// Ustawienie nagłówka z tokenem JWT
 const setAuthToken = token => {
   if (token) {
     axios.defaults.headers.common.Authorization = `Bearer ${token}`;
@@ -227,21 +19,9 @@ export const loginUser = createAsyncThunk(
       const token = response.data.token;
       localStorage.setItem('token', token);
       setAuthToken(token);
-      if (response.status === 200) {
-        const user = response.data.user;
-        dispatch(loginUserSuccess(user));
-      } else {
-        dispatch(loginUserFailure('Login failed'));
-      }
+      return response.data.user; // Zwracamy użytkownika
     } catch (error) {
-      if (!error.response) {
-        return rejectWithValue('No response from server');
-      }
-      const status = error.response.status;
-      if (status === 401 || status === 403) {
-        return rejectWithValue('Unauthorized access');
-      }
-      return rejectWithValue(error.message);
+      throw error;
     }
   }
 );
@@ -251,24 +31,12 @@ export const registerUser = createAsyncThunk(
   async (userData, { dispatch, rejectWithValue }) => {
     try {
       const response = await axios.post(`${apiUrl}/signup`, userData);
-      if (response.status === 201) {
-        const user = response.data.user;
-        const token = response.data.token;
-        localStorage.setItem('token', token);
-        setAuthToken(token);
-        dispatch(loginUserSuccess(user));
-      } else {
-        dispatch(loginUserFailure('Registration failed'));
-      }
+      const token = response.data.token;
+      localStorage.setItem('token', token);
+      setAuthToken(token);
+      return response.data.user;
     } catch (error) {
-      if (!error.response) {
-        return rejectWithValue('No response from server');
-      }
-      const status = error.response.status;
-      if (status === 401 || status === 403) {
-        return rejectWithValue('Unauthorized access');
-      }
-      return rejectWithValue(error.message);
+      throw error;
     }
   }
 );
@@ -278,18 +46,13 @@ export const refreshToken = createAsyncThunk(
   async (_, { dispatch, getState, rejectWithValue }) => {
     const token = getState().auth.token;
     if (!token) {
-      return rejectWithValue('No token available');
+      throw new Error('No token available');
     }
     try {
       const response = await axios.get(`${apiUrl}/current`);
-      if (response.status === 200) {
-        const user = response.data.user;
-        dispatch(loginUserSuccess(user));
-      } else {
-        dispatch(logoutUser());
-      }
+      return response.data.user;
     } catch (error) {
-      dispatch(logoutUser());
+      throw error;
     }
   }
 );
@@ -298,17 +61,11 @@ export const logoutUser = createAsyncThunk(
   'auth/logoutUser',
   async (_, { rejectWithValue }) => {
     try {
-      const response = await axios.post(`${apiUrl}/logout`);
-      return response.data;
+      await axios.post(`${apiUrl}/logout`);
+      localStorage.removeItem('token');
+      return null;
     } catch (error) {
-      if (!error.response) {
-        return rejectWithValue('No response from server');
-      }
-      const status = error.response.status;
-      if (status === 401 || status === 403) {
-        return rejectWithValue('Unauthorized access');
-      }
-      return rejectWithValue(error.message);
+      throw error;
     }
   }
 );
@@ -322,85 +79,61 @@ const authSlice = createSlice({
     loading: false,
     error: null,
   },
-  reducers: {
-    loginUserSuccess(state, action) {
-      state.user = action.payload;
-      state.isLoggedIn = true;
-      state.loading = false;
-      state.error = null;
-    },
-    loginUserFailure(state, action) {
-      state.user = null;
-      state.isLoggedIn = false;
-      state.loading = false;
-      state.error = action.payload;
-    },
-    logoutUserSuccess(state) {
-      state.isLoggedIn = false;
-      state.user = null;
-      state.error = null;
-    },
-    logoutUserFailure(state, action) {
-      state.error = action.payload;
-    },
-  },
+  reducers: {},
   extraReducers: builder => {
     builder
       .addCase(loginUser.pending, state => {
         state.loading = true;
         state.error = null;
       })
-      .addCase(loginUser.fulfilled, state => {
+      .addCase(loginUser.fulfilled, (state, action) => {
         state.loading = false;
+        state.isLoggedIn = true;
+        state.user = action.payload;
       })
       .addCase(loginUser.rejected, (state, action) => {
         state.loading = false;
-        state.error = action.payload;
+        state.error = action.error.message;
       })
       .addCase(registerUser.pending, state => {
         state.loading = true;
         state.error = null;
       })
-      .addCase(registerUser.fulfilled, state => {
+      .addCase(registerUser.fulfilled, (state, action) => {
         state.loading = false;
+        state.isLoggedIn = true;
+        state.user = action.payload;
       })
       .addCase(registerUser.rejected, (state, action) => {
         state.loading = false;
-        state.error = action.payload;
+        state.error = action.error.message;
       })
       .addCase(refreshToken.pending, state => {
         state.loading = true;
         state.error = null;
       })
-      .addCase(refreshToken.fulfilled, state => {
+      .addCase(refreshToken.fulfilled, (state, action) => {
         state.loading = false;
+        state.user = action.payload;
       })
       .addCase(refreshToken.rejected, (state, action) => {
         state.loading = false;
-        state.error = action.payload;
+        state.error = action.error.message;
       })
       .addCase(logoutUser.pending, state => {
         state.loading = true;
         state.error = null;
       })
-      .addCase(logoutUser.fulfilled, state => {
+      .addCase(logoutUser.fulfilled, (state, action) => {
         state.loading = false;
         state.isLoggedIn = false;
         state.user = null;
-        state.error = null;
       })
       .addCase(logoutUser.rejected, (state, action) => {
         state.loading = false;
-        state.error = action.payload;
+        state.error = action.error.message;
       });
   },
 });
-
-export const {
-  loginUserSuccess,
-  loginUserFailure,
-  logoutUserSuccess,
-  logoutUserFailure,
-} = authSlice.actions;
 
 export default authSlice.reducer;
